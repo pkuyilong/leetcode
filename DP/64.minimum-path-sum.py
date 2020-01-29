7b7df6a5adb7aa8000de3aa7557cb6f24e8baf0c
mayilong <mayilong@pku.edu.cn>
2020-01-29 12:35:28 +0800

    Add 72.edit-distance.py

--- DP/64.minimum-path-sum.py
+++ DP/64.minimum-path-sum.py
@@ -34,15 +34,17 @@ class Solution:
         if len(grid) == 1:
             return sum(grid[0])
         if len(grid[0]) == 1:
-            return sum([tmp_list[0] for tmp_list in l])
-        for i in range(1, len(grid)):
+            return sum([tmp_list[0] for tmp_list in grid])
+
+        m, n = len(grid), len(grid[0])
+        for i in range(1, m):
             grid[i][0] = grid[i - 1][0] + grid[i][0]
-        for i in range(1, len(grid[0])):
+        for i in range(1, n):
             grid[0][i] = grid[0][i - 1] + grid[0][i]
-        for i in range(1, len(grid)):
-            for j in range(1, len(grid[0])):
+        for i in range(1, m):
+            for j in range(1, n):
                 grid[i][j] = min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j]
-        return grid[len(grid) - 1][len(grid[0]) - 1]
+        return grid[m - 1][n - 1]
 
 
 if __name__ == '__main__':
---
+++ DP/72.edit-distance.py
@@ -0,0 +1,70 @@
+# 72. 编辑距离
+# https://leetcode-cn.com/problems/edit-distance/
+
+"""
+给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。
+
+你可以对一个单词进行如下三种操作：
+
+插入一个字符
+删除一个字符
+替换一个字符
+示例 1:
+
+输入: word1 = "horse", word2 = "ros"
+输出: 3
+解释:
+horse -> rorse (将 'h' 替换为 'r')
+rorse -> rose (删除 'r')
+rose -> ros (删除 'e')
+
+
+
+字符的问题，一般都是动态规划问题，而且一般与dp[i][j], dp[i-1][j], dp[i][j-1]有关
+
+
+dp[i][j]表示当word1在i位置， word2在j位置时，最小的改动次数
+1. 如果字符不同：
+    1.1 可以在第一个单词后边插入一个字符
+        dp[i-1][j]
+    1.2 可以在第二个单词中删除一个字符
+        dp[i][j-1]
+    1.3 可以将第一个单词的i位置字符换成第二个单词j位置的字符
+        dp[i-1][j-1]
+    总的公式就是  dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+
+2. 如果字符相同：
+    dp[i][j] = dp[i-1][j-1]
+"""
+
+from pprint import pprint
+
+class Solution(object):
+    def minDistance(self, word1, word2):
+        if word1 is None or word2 is None:
+            return 0
+        w1_len, w2_len = len(word1), len(word2)
+        if w1_len == 0 or w2_len == 0:
+            return w1_len if w2_len == 0 else w2_len
+        dp = [[0 for i in range(w2_len + 1)] for j in range(w1_len + 1)]
+        for i in range(w2_len + 1):
+            dp[0][i] = i
+        for i in range(w1_len + 1):
+            dp[i][0] = i
+        for i in range(1, w1_len + 1):
+            for j in range(1, w2_len + 1):
+                if word1[i-1] != word2[j-1]:
+                    dp[i][j] = min(dp[i-1][j-1],
+                                   dp[i][j-1],
+                                   dp[i-1][j]) + 1
+                else:
+                    dp[i][j] = dp[i-1][j-1]
+        return dp[w1_len][w2_len]
+
+
+if __name__ == '__main__':
+    word1 = "a"
+    word2 = "b"
+    sol = Solution()
+    res = sol.minDistance(word1, word2)
+    print(res)
