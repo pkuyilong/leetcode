d1cc262482dd05cd0f442a6d1ac9cc420f3accd5
mayilong <mayilong@pku.edu.cn>
2020-01-30 13:17:55 +0800

    Add 72.edit-distance.py version 2

--- DP/72.edit-distance.py
+++ DP/72.edit-distance.py
@@ -37,9 +37,8 @@ dp[i][j]表示当word1在i位置， word2在j位置时，最小的改动次数
     dp[i][j] = dp[i-1][j-1]
 """
 
-from pprint import pprint
 
-class Solution(object):
+class Solution_0:
     def minDistance(self, word1, word2):
         if word1 is None or word2 is None:
             return 0
@@ -53,18 +52,46 @@ class Solution(object):
             dp[i][0] = i
         for i in range(1, w1_len + 1):
             for j in range(1, w2_len + 1):
-                if word1[i-1] != word2[j-1]:
-                    dp[i][j] = min(dp[i-1][j-1],
-                                   dp[i][j-1],
-                                   dp[i-1][j]) + 1
+                if word1[i - 1] != word2[j - 1]:
+                    dp[i][j] = min(dp[i - 1][j - 1],
+                                   dp[i][j - 1],
+                                   dp[i - 1][j]) + 1
                 else:
-                    dp[i][j] = dp[i-1][j-1]
+                    dp[i][j] = dp[i - 1][j - 1]
+        for item in dp:
+            print(item)
         return dp[w1_len][w2_len]
 
 
+class Solution:
+    def minDistance(self, word1, word2):
+        if word1 is None or word2 is None:
+            return 0
+        w1_len, w2_len = len(word1), len(word2)
+        if w1_len == 0 or w2_len == 0:
+            return w1_len if w2_len == 0 else w2_len
+
+        dp = [i for i in range(w2_len + 1)]
+        print(dp)
+        for i in range(1, w1_len+1):
+            tmp = dp[0]
+            dp[0] = i
+            for j in range(1, w2_len+1):
+                # 先保存下来dp之前的数值
+                pre = tmp
+                # 因为马上就要更新dp[j]，所以提前保留下来
+                tmp = dp[j]
+                if word1[i-1] != word2[j-1]:
+                    dp[j] = min(min(dp[j-1], dp[j]), pre) + 1
+                else:
+                    dp[j] = pre
+            print(dp)
+        return dp[w2_len]
+
+
 if __name__ == '__main__':
-    word1 = "a"
-    word2 = "b"
-    sol = Solution()
+    word1 = "horse"
+    word2 = "ros"
+    sol = Solution_0()
     res = sol.minDistance(word1, word2)
     print(res)
