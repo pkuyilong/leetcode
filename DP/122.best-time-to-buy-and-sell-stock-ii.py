64aa4dd7fd4c142bee28fa2bae00de0b6afe79dd
mayilong <mayilong@pku.edu.cn>
2020-02-02 19:02:30 +0800

    DP/53.maximum-subarray.py DP/121.best-time-to-buy-and-sell-stock.py  DP/122.best-time-to-buy-and-sell-stock-ii.py

---
+++ DP/122.best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,45 @@
+# 122. 买卖股票的最佳时机 II
+# https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+
+"""思路
+    不同于题目121，这道题目强调可以多次买卖，也就是有利益差就可以执行交易
+    但是这道题并不关系你如何交易，只需要知道最大的利益就可以
+    prices = [7, 1, 5, 3, 6, 4]
+    首先计算出相邻两天价格的价差
+    diff   =     -6， 4， -2， 3， -2
+    所以只需要关注大于0的数值就可以，正数可以代表盈利
+
+
+
+    可能有人会有这样的疑问 因为题目规定了不可以同时开始多笔交易
+    prices = [1, 4, 7, 8, 5, 2]
+    首先计算出相邻两天价格的价差
+    diff   =    3, 3, 1, -3, -3
+    为了得到第一个3，要在第一天买入，第二天卖出，但是因为条件限制，卖出的当天不可以买进股票
+    所以第二个3是怎么得到的呢？
+    其实你可以在第一天买入，第二天不卖出，第三天不卖出，知道第四天才卖出就可以顺利的赚到
+    3 + 3 + 1 = 8 - 1
+    所以只需要关注大于0的数值就可以，正数可以代表盈利
+
+"""
+
+
+class Solution:
+    def maxProfit(self, prices):
+        if not prices or len(prices) == 0:
+            return 0
+        total = 0
+        diff = []
+        for i in range(1, len(prices)):
+            diff.append(prices[i] - prices[i - 1])
+        for di in diff:
+            if di > 0:
+                total += di
+        return total
+
+
+if __name__ == '__main__':
+    prices = [7, 1, 5, 3, 6, 4]
+    res = Solution().maxProfit(prices)
+    print(res)
--- DP/53.maximum-subarray.py
+++ DP/53.maximum-subarray.py
@@ -1,8 +1,28 @@
 # 53. 最大子序和
 # https://leetcode-cn.com/problems/maximum-subarray/
 
+""" 思路
+    dp[i] 代表考虑到第i个位置为止，最大的子序列长度之和
+    但是该题目并不是dp数组的最后一个结果为最大值，需要额外使用一个变量来不断更新最大值
 
+"""
+
+
+# 53题解
 class Solution:
+    def maxSubArray(self, nums):
+        if not nums or len(nums) == 0: return nums
+        dp = [0 for i in range(len(nums))]
+        dp[0] = nums[0]
+        max_res = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1], 0) + nums[i]
+            max_res = max(max_res, dp[i])
+        return max_res
+
+
+# 升级版：如果要计算出最大子序列的起始位置， 用这个代码
+class Solution_0:
     def maxSubArray(self, nums):
         if not nums or len(nums) == 0:
             return 0
@@ -11,9 +31,24 @@ class Solution:
         dp = [0 for i in range(len(nums))]
         dp[0] = nums[0]
         max_res = nums[0]
+
+        tmp_pos = 0
+        start_pos = 0
+        end_pos = 0
         for i in range(1, len(nums)):
-            dp[i] = max(0, dp[i - 1]) + nums[i]
-            max_res = max(max_res, dp[i])
+            if dp[i - 1] < 0:
+                # 记录当前是从哪个位置开始的，但是从该位置开始的连续子序列不一定是最大的
+                tmp_pos = i
+                dp[i] = nums[i]
+            else:
+                dp[i] = dp[i - 1] + nums[i]
+
+            # 如果出现了新的最大值，那么同时更新起始位置
+            if dp[i] > max_res:
+                max_res = dp[i]
+                end_pos = i
+                start_pos = tmp_pos
+        print(start_pos, end_pos)
         return max_res
 
 
