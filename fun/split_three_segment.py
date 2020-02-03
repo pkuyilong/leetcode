639c5276d6e71f6d350c69309f916f92e8bb830b
mayilong <mayilong@pku.edu.cn>
2020-02-03 16:30:42 +0800

    DP/343.integer-break.py

--- fun/split_three_segment.py
+++ fun/split_three_segment.py
@@ -4,26 +4,30 @@
 class Solution():
     def split_segment(self, num, k):
         tmp = []
+        final = []
         tmp_count = 0
-        self.helper(num, k, tmp_count, tmp)
+        self.helper(num, k, tmp_count, tmp, final)
+        return final
 
-    def helper(self, num, k, tmp_count, tmp):
+    def helper(self, num, k, tmp_count, tmp, final):
         if num < 0 or tmp_count > k:
             return
         if tmp_count == k and num == 0:
-            print(tmp)
-            return
+            final.append(tmp[:])
+            return final
 
         for i in range(1, num+1):
+            if len(tmp) and i < tmp[-1]:
+                continue
             tmp.append(i)
-            # print(tmp)
-            self.helper(num - i, k, tmp_count + 1, tmp)
+            self.helper(num - i, k, tmp_count + 1, tmp, final)
             tmp.pop()
 
 
 if __name__ == '__main__':
     sol = Solution()
-    sol.split_segment(8, 3)
+    res = sol.split_segment(8, 3)
+    print(res)
 
 
 
