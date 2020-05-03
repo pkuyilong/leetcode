bf9f4c8dc0117184f27385c7112b502afff51b6f
mayilong <pkumarlon@gmail.com>
2020-05-03 19:42:55 +0800

    update

--- DP/122.best-time-to-buy-and-sell-stock-ii.py
+++ DP/122.best-time-to-buy-and-sell-stock-ii.py
@@ -25,18 +25,42 @@
 """
 
 
+# class Solution:
+#     def maxProfit(self, prices):
+#         if not prices or len(prices) == 0:
+#             return 0
+#         total = 0
+#         diff = []
+#         for i in range(1, len(prices)):
+#             diff.append(prices[i] - prices[i - 1])
+#         for di in diff:
+#             if di > 0:
+#                 total += di
+#         return total
+
+
 class Solution:
     def maxProfit(self, prices):
-        if not prices or len(prices) == 0:
+        if prices is None or len(prices) == 0:
             return 0
-        total = 0
-        diff = []
-        for i in range(1, len(prices)):
-            diff.append(prices[i] - prices[i - 1])
-        for di in diff:
-            if di > 0:
-                total += di
-        return total
+
+        vally = prices[0]
+        peak = prices[0]
+        res = 0
+        i = 0
+        while i < len(prices) - 1:
+            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+                i += 1
+            vally = prices[i]
+            print("vally is {}".format(vally))
+            i += 1
+
+            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
+                i += 1
+            peak = prices[i]
+            print("peak is {}".format(peak))
+            res = res + peak - vally
+        return res
 
 
 if __name__ == '__main__':
