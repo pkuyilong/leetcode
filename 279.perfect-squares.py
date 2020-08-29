2e7dc1b5399b4730c27df3757a9ebeceac3ba815
mayilong <mayilong@pku.edu.cn>
2020-08-29 20:06:33 +0800

    update

--- 279.perfect-squares.py
+++ 279.perfect-squares.py
@@ -18,8 +18,8 @@
 """
 
 
+import sys
 class Solution:
-    import sys
     def numSquares(self, n):
         if n < 0:
             return -1
@@ -50,20 +50,19 @@ class Solution:
 #         return dp[n]
 
 
-class Solution:
-    def numSquares(self, n):
-        import math
-        import sys
-        if n < 0:
-            return -1
-        dp = [sys.maxsize for i in range(n + 1)]
-        dp[0] = 0
-        for i in range(1, n + 1):
-            # 这种写法不需要一次一次的计算平方值
-            for j in range(1, int(math.sqrt(i))+ 1):
-                dp[i] = min(dp[i - j * j] + 1, dp[i])
-        return dp[n]
-
+# class Solution:
+#     def numSquares(self, n):
+#         import math
+#         import sys
+#         if n < 0:
+#             return -1
+#         dp = [sys.maxsize for i in range(n + 1)]
+#         dp[0] = 0
+#         for i in range(1, n + 1):
+#             # 这种写法不需要一次一次的计算平方值
+#             for j in range(1, int(math.sqrt(i))+ 1):
+#                 dp[i] = min(dp[i - j * j] + 1, dp[i])
+#         return dp[n]
 
 
 """ 思路
@@ -73,20 +72,77 @@ class Solution:
     1    2
 0      
 """
-from collections import deque
+
+# from collections import deque
+# import math
+# class Solution:
+#     def numSquares(self, n):
+#         if n <= 0:
+#             return -1
+#         q = deque()
+#         q.append((n, 0))
+#         while len(q):
+#             tmp_node = q.popleft()
+#             value, level = tmp_node
+#             for j in range(int(math.sqrt(value)), -1, -1):
+#                 if value - j * j > 0:
+#                     q.append((value - j * j, level + 1))
+#                 if value - j * j == 0:
+#                     return level + 1
+
+
+### timeout版本, 没办法使用备忘录递归
+# import math
+# class Solution:
+#     count = 0
+#     mem = dict()
+#
+#     def numSquares(self, n):
+#         self.count = n
+#         if n <= 0:
+#             return 0
+#         if n == 1:
+#             return 1
+#         self.helper(n, 0)
+#         return self.count
+#
+#     def helper(self, n, depth):
+#         if n < 0:
+#             return
+#         if n == 0:
+#             self.count = min(self.count, depth)
+#             return
+#
+#         for i in range(int(math.sqrt(n)), 0, -1):
+#             if i * i <= n:
+#                 self.helper(n - i * i, depth + 1)
+
+
 import math
 class Solution:
-    def numSquares(self, n):
-        if n <= 0:
-            return -1
-        q = deque()
-        q.append((n, 0))
-        while len(q):
-            tmp_node = q.popleft()
-            value, level = tmp_node
-            for j in range(int(math.sqrt(value)), -1, -1):
-                if value - j * j > 0:
-                    q.append((value - j * j, level + 1))
-                if value - j * j == 0:
-                    return level + 1
+    mem = dict()
 
+    def numSquares(self, n):
+        for i in range(n+1):
+            self.mem[i] = i
+
+        res = self.helper(n)
+        return res
+
+    def helper(self, n):
+        if self.mem[n] != n:
+            return self.mem[n]
+
+        if n == 0:
+            return 0
+
+        for i in range(int(math.sqrt(n)), 0, -1):
+            if i * i <= n:
+                self.mem[n] = min(self.mem[n], self.helper(n - i * i) + 1)
+        return self.mem[n]
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    res = sol.numSquares(43)
+    print(res)
