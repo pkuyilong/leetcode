1165af6f3ab1501dc5f8e69174f047a9a8dec368
mayilong <pkumarlon@gmail.com>
2020-09-06 17:54:13 +0800

    update

--- 28.implement-strstr.py
+++ 28.implement-strstr.py
@@ -20,3 +20,29 @@ class Solution(object):
             if j == len(needle):
                 return i
         return -1
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if haystack is None:
+            return -1
+        if needle == "":
+            return 0
+
+        ### 最有一个有意义的位置: len(stack) - 1  - len(needle) + 1
+        ### 然后还要 + 1 (最有的一个+1是因为要放在range里面, 不然最有一个有意义的位置取不到)
+        for i in range(len(haystack) - len(needle) + 1):
+            # 这个j是为了遍历needle而准备的
+            j = 0
+            while j < len(needle):
+                if haystack[i+j] != needle[j]:
+                    break
+                j +=1
+            if j == len(needle):
+                return i
+        return -1
